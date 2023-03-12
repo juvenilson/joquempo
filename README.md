b09c82ecae37111af5b8ec740f6db72b7790f1cd
juvess <juvenilsonsousa78@gmail.com>
2023-03-12 18:17:18 -0300

    commitando as modificacoes simples

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 # joquempo
 >> Gaming with machine.<br>
-  >> This is a simple game to practice my python coding.</br>
+>> This is a simple game to practice my python coding.</br>
 >> Try to play with you want.<br>
 
 </body>
--- joquempo.py
+++ joquempo.py
@@ -1,15 +1,23 @@
+import emoji 
 from random import choice
 from time import sleep
-import emoji 
-
 
 def show_who_win(choice_by_pc):
+    """Função para retornar o resultado do jogo prntando na tela quais foram as escolhas, e trazendo 
+    os emojis respectivos.
+    """
     print(f"You win!!! i chose {choices_to_emoji(choice_by_pc)}, and you {choices_to_emoji(player_choice)}.")
 
+
 def show_who_lose(choice_by_pc):
+    """Função para printar o resultado do jogo trazendo os respectivos emojis.
+    """
     print(f"You lose!! i chose {choices_to_emoji(choice_by_pc)}, and you {choices_to_emoji(player_choice)}.")
 
+
 def choices_to_emoji(choice):
+    """Função para selecionar o emoji a ser apresentado em tela como resultado das escohas.
+    """
     match(choice):
         case "rock":
             return emoji.emojize(":right-facing_fist:")
@@ -17,23 +25,19 @@ def choices_to_emoji(choice):
             return emoji.emojize(":hand_with_fingers_splayed:")
         case "scissor":
             return emoji.emojize( ":vulcan_salute:")
-
-
+            
 
 possibilities_to_choose = ["rock", "paper", "scissor"]
 
-choice_by_pc = ""
-player_choice = ""
-
-while player_choice != "end":
-    
+while player_choice:="" != "end":
     choice_by_pc = choice(possibilities_to_choose)
-    print(choice_by_pc)
     player_choice = str(input("pick one! rock, paper or scissor ")).lower().strip("")
-
     print("="*20 + "Joquempo" + "="*20)
     sleep(1)
-
+    if player_choice not in possibilities_to_choose:
+        print("You need to chose right, the possibilties are: rock, pape or scissor")
+        continue
+    
     match(choice_by_pc, player_choice):
         case["rock", "scissor"]:
             show_who_lose(choice_by_pc)        
@@ -49,3 +53,4 @@ while player_choice != "end":
             show_who_win(choice_by_pc)
         case[_, _]:
             print(f"We have a draw!!! I chose {choices_to_emoji(choice_by_pc)}, and you too!")
+        
