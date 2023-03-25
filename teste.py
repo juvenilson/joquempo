221ccd202f4b23329c23a28e960816cbe62e7d04
juvess <juvenilsonsousa78@gmail.com>
2023-03-25 11:00:37 -0300

    troca do nome das variaveis locais da def.

--- teste.py
+++ teste.py
@@ -1,13 +1,15 @@
-import emoji 
 from random import choice
 from time import sleep
 
-def show_the_result(win=0, lose=0):
+import emoji
+
+
+def show_the_result(res1, res2, win):
     """This function shows the results of the game, printing the result of the choices by computer
     and player.
     Attention, this function needs to be accompained by the choices_to_emojis function.
 
-    Exemple:
+    Exempla:
         when 'whin' parameter receives number 1, the function print that the play is winner.
         However the parameter lose receive the number 1, the it'll print the computer(PC) is the winner.
 
@@ -19,10 +21,9 @@ def show_the_result(win=0, lose=0):
         no returns.
         
     """
-    if win == 1:
-        print(f"You win!!! i chose {choices_to_emoji(choice_by_pc)}, and you {choices_to_emoji(player_choice)}.")
-    if lose == 1:
-        print(f"You lose!! i chose {choices_to_emoji(choice_by_pc)}, and you {choices_to_emoji(player_choice)}.")
+    if win:
+        return f"You win!!! i chose {choices_to_emoji(res1)}, and you {choices_to_emoji(res2)}."
+    return f"You lose!! i chose {choices_to_emoji(res1)}, and you {choices_to_emoji(res2)}."
 
 
 def choices_to_emoji(choice):
@@ -38,6 +39,7 @@ def choices_to_emoji(choice):
     returns:
         Retuns the usable emoji[punsh, high-five hand and vulcan salute from emoji library] to put
         in any print or frame.
+
     """
     match(choice):
         case "rock":
@@ -50,28 +52,21 @@ def choices_to_emoji(choice):
 
 possibilities_to_choose = ["rock", "paper", "scissor"]
 
-while player_choice:="" != "end":
+while player_choice:= "" != "end":
     choice_by_pc = choice(possibilities_to_choose)
     player_choice = str(input("pick one! rock, paper or scissor ")).lower().strip("")
     print("="*20 + "Joquempo" + "="*20)
     sleep(1)
+    
     if player_choice not in possibilities_to_choose:
         print("You need to chose right, the possibilties are: rock, pape or scissor")
-        continue
-    
+        break
+
     match(choice_by_pc, player_choice):
-        case["rock", "scissor"]:
-            show_the_result(lose=1)        
-        case["paper", "rock"]:
-            show_the_result(lose=1)
-        case["scissor", "paper"]:
-            show_the_result(lose=1)
-        case["rock", "paper"]:
-            show_the_result(win=1)
-        case["paper", "scissor"]:
-            show_the_result(win=1)
-        case["scissor", "rock"]:
-            show_the_result(win=1)
+        case ["rock", "scissor"] | ["scissor", "paper"] | ["paper", "rock"]:
+            print(show_the_result(choice_by_pc, player_choice, False))
+        case ["rock", "paper"] | ["paper", "scissor"] | ["scissor", "rock"]:
+            print(show_the_result(choice_by_pc, player_choice, True))
         case[_, _]:
             print(f"We have a draw!!! I chose {choices_to_emoji(choice_by_pc)}, and you too!")
         
