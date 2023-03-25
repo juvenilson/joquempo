e5d29833ded2917b5fbf03e405b49b7d333d921c
juvess <juvenilsonsousa78@gmail.com>
2023-03-25 10:55:54 -0300

    reduzindo as def em linhas de codigo

--- joquempo.py
+++ joquempo.py
@@ -1,22 +1,45 @@
-import emoji 
 from random import choice
 from time import sleep
 
-def show_who_win(choice_by_pc):
-    """Função para retornar o resultado do jogo printando na tela quais foram as escolhas, e trazendo 
-    os emojis respectivos.
-    """
-    print(f"You win!!! i chose {choices_to_emoji(choice_by_pc)}, and you {choices_to_emoji(player_choice)}.")
+import emoji
 
 
-def show_who_lose(choice_by_pc):
-    """Função para printar o resultado do jogo trazendo os respectivos emojis.
+def show_the_result(res1, res2, win):
+    """This function shows the results of the game, printing the result of the choices by computer
+    and player.
+    Attention, this function needs to be accompained by the choices_to_emojis function.
+
+    Exempla:
+        when 'whin' parameter receives number 1, the function print that the play is winner.
+        However the parameter lose receive the number 1, the it'll print the computer(PC) is the winner.
+
+    Args:
+        win (str): parameter from match comparison.
+        lose (str): parameter from match comparison.
+
+    Return: 
+        no returns.
+        
     """
-    print(f"You lose!! i chose {choices_to_emoji(choice_by_pc)}, and you {choices_to_emoji(player_choice)}.")
+    if win:
+        return f"You win!!! i chose {choices_to_emoji(res1)}, and you {choices_to_emoji(res2)}."
+    return f"You lose!! i chose {choices_to_emoji(res1)}, and you {choices_to_emoji(res2)}."
 
 
 def choices_to_emoji(choice):
-    """Função para selecionar o emoji a ser apresentado em tela como resultado das escohas.
+    """This function returns the emojis to be used to show them.
+
+    Exemple:
+        if it's receive the 'rock' word it'll retunr ':right-facing_fist:' and with the emojize's
+        function help it's shows the punch emoji.
+
+    Args:
+        choice (str): receive the argument from input or random lists.
+
+    returns:
+        Retuns the usable emoji[punsh, high-five hand and vulcan salute from emoji library] to put
+        in any print or frame.
+
     """
     match(choice):
         case "rock":
@@ -29,28 +52,21 @@ def choices_to_emoji(choice):
 
 possibilities_to_choose = ["rock", "paper", "scissor"]
 
-while player_choice:="" != "end":
+while player_choice:= "" != "end":
     choice_by_pc = choice(possibilities_to_choose)
     player_choice = str(input("pick one! rock, paper or scissor ")).lower().strip("")
     print("="*20 + "Joquempo" + "="*20)
     sleep(1)
+    
     if player_choice not in possibilities_to_choose:
         print("You need to chose right, the possibilties are: rock, pape or scissor")
-        continue
-    
+        break
+
     match(choice_by_pc, player_choice):
-        case["rock", "scissor"]:
-            show_who_lose(choice_by_pc)        
-        case["paper", "rock"]:
-            show_who_lose(choice_by_pc)
-        case["scissor", "paper"]:
-            show_who_lose(choice_by_pc)
-        case["rock", "paper"]:
-            show_who_win(choice_by_pc)
-        case["paper", "scissor"]:
-            show_who_win(choice_by_pc)
-        case["scissor", "rock"]:
-            show_who_win(choice_by_pc)
+        case ["rock", "scissor"] | ["scissor", "paper"] | ["paper", "rock"]:
+            print(show_the_result(choice_by_pc, player_choice, False))
+        case ["rock", "paper"] | ["paper", "scissor"] | ["scissor", "rock"]:
+            print(show_the_result(choice_by_pc, player_choice, True))
         case[_, _]:
             print(f"We have a draw!!! I chose {choices_to_emoji(choice_by_pc)}, and you too!")
         
